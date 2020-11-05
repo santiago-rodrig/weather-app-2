92954155272550e1782675f844a0614e10a02f3d
Santiago <santo1996.29@gmail.com>
2020-11-05 18:11:13 -0400

    feature: Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './components/Header'
 
 function App() {
   return (
-    <h1>App component</h1>
+    <>
+      <Header>Aplicación del Clima</Header>
+    </>
   );
 }
 
